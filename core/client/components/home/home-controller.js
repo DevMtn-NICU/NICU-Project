ba43084dc578bd77f4249ed6e6d7f809b3971bf8
BrendanDayley <brendandayley@gmail.com>
2015-09-11 14:28:37 -0600

    tied in the home controller

--- core/client/components/home/home-controller.js
+++ core/client/components/home/home-controller.js
@@ -1,10 +1,19 @@
 (function () {
-    "use strict";
+   "use strict";
 
-    angular.module('app')
-        .controller('homeController', function ($scope) {
-            $scope.test = "test";
-        });
+   angular.module('app')
+      .controller('homeController', function ($scope) {
+         $scope.test = "blah blah blah";
+
+         $scope.nurseLogin = function () {
+            alert("you have been upgraded from patient to doctor, now for surgery, don't worry you can do it just cut along the dotted line");
+         }
+
+         $scope.parentLogin = function () {
+            alert("FACT: the nurse instantiated your baby");
+         }
+
+      });
 
 
-} ());
+}());
