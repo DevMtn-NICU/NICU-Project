db51a514e49c020daf0f0041ac033d413dd37703
Wilson Parrish <wilsonparrishmk2@gmail.com>
2015-09-11 11:22:06 -0600

    created baby model

---
+++ core/server/models/baby.server.model.js
@@ -0,0 +1,27 @@
+// baby model
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var babySchema = new Schema({
+    firstName: String,
+	middleName: String,
+    lastName: String,
+	
+	nurses: [{ type: Schema.Types.ObjectId, ref: 'User' }], //this might not be necessary?
+	parents: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    notes: [{ type: Schema.Types.ObjectId, ref: 'Note' }],
+    
+    level1: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    level2: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    level3: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    
+    birthDate: Date,
+    dischargeDate: Date, 
+    
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Baby', babySchema);
