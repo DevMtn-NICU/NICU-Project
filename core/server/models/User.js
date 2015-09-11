d5b1d53175079d736f066b2771a71985f9cce461
Spencer Davies <spencerhdavies@gmail.com>
2015-09-11 11:26:52 -0600

    added better notes for user, removed unnecessary packages from package.json

--- core/server/models/User.js
+++ core/server/models/User.js
@@ -5,6 +5,7 @@ var mongoose = require('mongoose'),
 
 var userSchema = new Schema ({
   role: {type: String, required: true, enum: ["parent", "nurse", "contact"]},
+  //match checks for valid email with "@" and "."
   email: {type: String, match: [/.+\@.+\..+/, "Please fill a valid e-mail address"], required: true},
   password: {type: String, required: true},
   nurse: {
@@ -13,12 +14,15 @@ var userSchema = new Schema ({
   parent: {
     access: {type: String, enum: ["parent"]},
     baby: {type: Schema.Types.ObjectId, ref: "Baby"}
-  }
+  },
+  created_at: {type: Date, default: Date.now()}
 });
 
+//Use this function when creating a user, hashes out the password
 userSchema.methods.generateHash = function(password) {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
+
 
 userSchema.methods.validPassword = function(password) {
   return bcrypt.compareSync(password, this.password);
