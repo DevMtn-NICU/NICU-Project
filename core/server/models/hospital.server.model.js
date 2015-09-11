fce8c584c68c08b99a298f7ba4b2400903243f57
Wilson Parrish <wilsonparrishmk2@gmail.com>
2015-09-11 16:04:41 -0600

    added required fields, and gender/deathDate to baby

--- core/server/models/hospital.server.model.js
+++ core/server/models/hospital.server.model.js
@@ -4,10 +4,10 @@ var mongoose = require('mongoose'),
 
 var hospitalSchema = new Schema({
     
-    name: String,
-    address: String,
-    phone: {type: Number, minlength: 7, maxlength: 10},  //formatting of the number needs to be done on front end
-    email: {type: String, match: [/.+\@.+\..+/, "Please fill a valid e-mail address"], required: true, unique: true},
+    name: { type: String, required: true },
+    address: { type: String, required: true },
+    phone: { type: Number, minlength: 7, maxlength: 10, required: true },  //formatting of the number needs to be done on front end
+    email: { type: String, match: [/.+\@.+\..+/, "Please fill a valid e-mail address"], required: true, unique: true},
     
     nurses: [{ type: Schema.Types.ObjectId, ref: 'User' }],
     users: [{ type: Schema.Types.ObjectId, ref: 'User' }],
