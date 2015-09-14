de066e32b1dfc287b772c56091666ac8a324db4b
Spencer Davies <spencerhdavies@gmail.com>
2015-09-14 11:29:38 -0600

    working on makeBaby ui

--- core/server/models/baby.server.model.js
+++ core/server/models/baby.server.model.js
@@ -6,24 +6,25 @@ var babySchema = new Schema({
     firstName: { type: String, required: true },
 	middleName: { type: String },
     lastName: { type: String, required: true },
-    
+
     gender: { type: String, enum: ["Male", "Female"], required: true },
-	
+    patientNumber: {type: Number, required: true},
+
 	nurses: [{ type: Schema.Types.ObjectId, ref: 'User' }], //this might not be necessary?
 	parents: [{ type: Schema.Types.ObjectId, ref: 'User' }],
     notes: [{ type: Schema.Types.ObjectId, ref: 'Note' }],
-    
+
     level1: [{ type: Schema.Types.ObjectId, ref: 'User' }],
     level2: [{ type: Schema.Types.ObjectId, ref: 'User' }],
     level3: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-    
+
     birthWeight: { type: String, required: true },
     birthLength: { type: String, required: true },
-    
+
     birthDate: { type: Date, required: true },
     dischargeDate: Date,
-    deathDate: Date, 
-    
+    deathDate: Date,
+
     createdAt: {
         type: Date,
         default: Date.now
