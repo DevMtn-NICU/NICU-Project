c5cf4722bc9d585fb2bef5351e3c9e94c7210a17
BrendanDayley <brendandayley@gmail.com>
2015-09-14 11:49:10 -0600

    console log the login data passed in to make sure its right

--- core/client/services/login-service.js
+++ core/client/services/login-service.js
@@ -1,6 +1,7 @@
 angular.module('app')
 .service('LoginService', function ($http, $q) {
    this.validateLogin = function (login) {
+      console.log(login)
       var defer = $q.defer();
       var url = '/URLGOES/HERE';
       $http({
