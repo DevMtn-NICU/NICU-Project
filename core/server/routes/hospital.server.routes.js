950e8b3d4556bacc2a12abd8d82c10900d231c99
Spencer Davies <spencerhdavies@gmail.com>
2015-09-14 09:54:51 -0600

    minor changes

--- core/server/routes/hospital.server.routes.js
+++ core/server/routes/hospital.server.routes.js
@@ -3,11 +3,11 @@ var hospitalsController = require('../controllers/hospital.server.controller.js'
 module.exports = function (app) {
     app.route('/api/createHospital')
         .post(hospitalsController.createHospital);
-        
+
     app.route('/api/hospitals')
         .get(hospitalsController.getHospitals);
-		
+
 	app.route('/api/hospitals:id')
-		.get(hospitalsController.getHospital)
+		.get(hospitalsController.getHospital);
 
 };
