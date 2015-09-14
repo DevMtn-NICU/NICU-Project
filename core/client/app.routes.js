af22e3adbd5fcddb12b74d7dc5159901a4501274
Miles Peterken <mpeterken43@yahoo.com>
2015-09-14 15:04:58 -0600

    changes to nurse path before lunch

--- core/client/app.routes.js
+++ core/client/app.routes.js
@@ -1,34 +1,49 @@
-(function () {
-   "use strict";
+(function() {
+	"use strict";
 
-   angular.module('app')
-      .config(function ($stateProvider, $urlRouterProvider) {
-         $urlRouterProvider.otherwise('/');
+	angular.module('app')
+		.config(function($stateProvider, $urlRouterProvider) {
+			$urlRouterProvider.otherwise('/');
 
-         $stateProvider
-            .state('home', {
-               url: '/',
-               templateUrl: 'components/home/home-template.html',
-               controller: 'homeController'
-            })
-            .state('medical', {
-               url: '/medical',
-               templateUrl: 'components/nurse/nurse-template.html',
-               controller: 'nurseController'
-            })
-            .state('medical.search', {
-               url: '/medical/search',
-               templateUrl: 'components/nurse/search_baby.html',
-               controller: 'nurseController'
-            })
-            .state('medical.create_account', {
-               url: '/medical/account',
-               templateUrl: 'components/nurse/create_account.html',
-               controller: 'nurseController'
-            })
+			$stateProvider
+				.state('home', {
+					url: '/',
+					templateUrl: 'components/home/home-template.html',
+					controller: 'homeController'
+				})
+				.state('medical', {
+					url: '/medical',
+					templateUrl: 'components/nurse/nurse-template.html',
+					controller: 'nurseController'
+				})
+				.state('medical.search', {
+					url: '/medical/search',
+					templateUrl: 'components/nurse/search_baby.html',
+					controller: 'nurseController'
+				})
+				.state('medical.create_account', {
+					url: '/medical/account',
+					templateUrl: 'components/nurse/create_account.html',
+					controller: 'nurseController'
+				})
+				.state('medical.create_note', {
+						url: '/medical/create_note:/id',
+						templateUrl: 'components/nurse/create_note.html',
+						controller: 'nurseController',
+						resolve: {
+							promised: function($http) {
+								return $http({
+										method: 'GET',
+										url: '/api/babies:id'
+									})
+									.then(function(response) {
+										console.log(response);
+									});
+							}
+					}
+				})
 
-
-      });
+		});
 
 
 }());
--- core/client/components/nurse/nurse-controller.js
+++ core/client/components/nurse/nurse-controller.js
@@ -2,8 +2,25 @@
     "use strict";
 
     angular.module('app')
-        .controller('nurseController', function ($scope) {
-            $scope.test = "test";
+        .controller('nurseController', function ($scope, $stateParams) {
+
+
+        $scope.notes = {};
+        $scope.notes.stats = {};
+
+        $scope.addBabyNote = function() {
+		nurseService.addBabyNote().
+		then(function(response) {
+
+
+        };
+
+
+
+
+
+
+
         });
 
 
