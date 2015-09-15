4a76fd0f08ac407c4fbeb4d188e4122bb96a5cc2
Miles Peterken <Miles@Miless-MacBook-Air.local>
2015-09-15 10:51:28 -0600

    changed nurse service intoan IFFE

--- core/client/services/nurse-service.js
+++ core/client/services/nurse-service.js
@@ -1,17 +1,20 @@
-angular.module('app')
-.service('NurseService', function ($http, $q) {
+(function() {
+	"use strict";
+	angular.module('app')
+	.service('NurseService', function($http, $q) {
 
-    this.addBabyNote = function(note) {
-        var deferred = $q.defer();
-		$http({
-			method: 'POST',
-			url: '/babyNote',
-            data: note
-		}).then(function(response) {
-			var results = response.data;
-			deferred.resolve(results);
-		});
-		return deferred.promise;
-	};
+		this.addBabyNote = function(note) {
+			var deferred = $q.defer();
+			$http({
+				method: 'POST',
+				url: '/babyNote',
+				data: note
+			}).then(function(response) {
+				var results = response.data;
+				deferred.resolve(results);
+			});
+			return deferred.promise;
+		};
+	})
 
-});
+} ());
