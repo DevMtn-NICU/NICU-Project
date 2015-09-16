e2dd476a54b923dc4af7617128a22aa80249507d
CodeMaster7 <italianstalian_2@yahoo.com>
2015-09-16 16:40:57 -0600

    commented out .parents for testing

--- core/client/components/nurse/nurse-edit-controller.js
+++ core/client/components/nurse/nurse-edit-controller.js
@@ -6,12 +6,12 @@
 
          $scope.showAdd = false;
          $scope.baby = promised;
-         $scope.parent1 = {};
-         $scope.parent2 = {};
-         $scope.parent1.name = promised.parents[0].name;
-         $scope.parent2.name = promised.parents[1].name;
-         $scope.parent1.email = promised.parents[0].email;
-         $scope.parent2.email = promised.parents[1].email;
+//         $scope.parent1 = {};
+//         $scope.parent2 = {};
+//         $scope.parent1.name = promised.parents[0].name;
+//         $scope.parent2.name = promised.parents[1].name;
+//         $scope.parent1.email = promised.parents[0].email;
+//         $scope.parent2.email = promised.parents[1].email;
 
          $scope.makeBaby = function () {
             makeBabySvc.editBaby($scope.baby, $scope.parent1, $scope.parent2)
