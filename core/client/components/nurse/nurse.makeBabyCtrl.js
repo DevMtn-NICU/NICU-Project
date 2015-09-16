7499468144406a3bf6b21116bdb34585227c89b6
CodeMaster7 <italianstalian_2@yahoo.com>
2015-09-16 11:02:15 -0600

    added showAdd  to true

--- core/client/components/nurse/nurse.makeBabyCtrl.js
+++ core/client/components/nurse/nurse.makeBabyCtrl.js
@@ -1,6 +1,7 @@
 (function () {
   "use strict";
   angular.module('app').controller('makeBabyCtrl', function ($scope, makeBabySvc) {
+      $scope.showAdd = true;
     $scope.makeBaby = function() {
       makeBabySvc.makeBaby($scope.baby, $scope.parent1, $scope.parent2)
       .then(function(response) {
