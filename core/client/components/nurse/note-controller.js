5d8110ffbd95edb1acf2f1d3613d4ca7684e75d5
Wilson Parrish <wilsonparrishmk2@gmail.com>
2015-09-16 11:32:03 -0600

    fixed directive functionality and prepping for merge conflict

--- core/client/components/nurse/note-controller.js
+++ core/client/components/nurse/note-controller.js
@@ -1,30 +1,21 @@
-(function() {
+(function () {
   "use strict";
 
   angular.module('app')
-    .controller('noteController', function($scope, promised, NurseService) {
+    .controller('noteController', function ($scope, promised, NurseService) {
 
       $scope.note = {};
       $scope.note.stats = {};
       $scope.theBaby = promised;
 
-      $scope.addBabyNote = function() {
-          console.log(promised._id);
-          console.log(typeof promised._id);
-        var details = {
-          baby: promised._id,
-          stats: {
-            bloodPressure: $scope.note.stats.bloodPressure,
-            heartRate: parseInt($scope.note.stats.heartRate),
-            oxygen: parseInt($scope.note.stats.oxygen),
-            weight: $scope.note.stats.weight
-          },
-          comment: $scope.note.comment
-        }
-        console.log("details: ", details);
-        NurseService.addBabyNote(details).
-        then(function(response) {})
+      $scope.addBabyNote = function () {
+        // console.log(promised._id);
+        // console.log(typeof promised._id);
+        $scope.note.baby = promised._id;
+        console.log("Notes: ", $scope.note);
+        NurseService.addBabyNote($scope.note).
+          then(function (response) { })
       };
     })
 
-}());
+} ());
