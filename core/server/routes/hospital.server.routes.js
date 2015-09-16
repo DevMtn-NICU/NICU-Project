b92449f9e348bd83dc3dba2d5f76bfb559df9aef
Spencer Davies <spencerhdavies@gmail.com>
2015-09-16 15:52:24 -0600

    pulling master

--- core/server/routes/hospital.server.routes.js
+++ core/server/routes/hospital.server.routes.js
@@ -10,4 +10,18 @@ module.exports = function (app) {
 	app.route('/api/hospitals:id')
 		.get(hospitalsController.getHospital);
 
+  app.route('/api/staff')
+    .get(hospitalsController.getAllStaff);
+
+  app.route('/api/staff')
+    .post(hospitalsController.createStaff);
+
+  app.route('/api/staff/:id')
+    .get(hospitalsController.getOneStaff);
+
+  app.route('/api/staff/:id')
+    .put(hospitalsController.editStaff);
+
+  app.route('/api/staff/:id')
+    .delete(hospitalsController.deleteStaff);
 };
