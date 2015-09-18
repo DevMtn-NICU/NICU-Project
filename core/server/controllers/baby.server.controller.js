30516b7bf9c057cc70a0bc65fce94f7d4947273f
Spencer Davies <spencerhdavies@gmail.com>
2015-09-18 11:49:45 -0600

    still working on edit baby, everything working except delete parent

--- core/server/controllers/baby.server.controller.js
+++ core/server/controllers/baby.server.controller.js
@@ -4,7 +4,6 @@ var User = require("../models/user.server.model.js");
 var Q = require('q');
 
 exports.makeBaby = function (req, res) {
-	console.log(req.body);
 	var babyId, parent1Id, parent2Id;
 	var newBaby = new Baby(req.body.baby);
 	(function findParent() {
@@ -12,27 +11,22 @@ exports.makeBaby = function (req, res) {
 		User.findOne({email: req.body.parent1.email}, function(err, parent) {
 			if (parent) {
 				parent1Id = parent._id;
-				console.log("14", parent1Id);
 				newBaby.parents.push(parent1Id);
 				deferred.resolve();
 			} else {
-				console.log("19");
 				deferred.resolve();
 			}
 		});
 		return deferred.promise;
 	}()).then(function() {
-		console.log("25");
 		var deferred = Q.defer();
 		if(req.body.parent2) {
 			User.findOne({email: req.body.parent2.email}, function(err, parent) {
 				if (parent) {
 					parent2Id = parent._id;
-					console.log("27", parent2Id);
 					newBaby.parents.push(parent2Id);
 					deferred.resolve();
 				} else {
-					console.log("33");
 					deferred.resolve();
 				}
 			});
@@ -45,21 +39,17 @@ exports.makeBaby = function (req, res) {
 		newBaby.save(function(err, baby) {
 			if (err) return res.status(500).send(err);
 			babyId = baby._id;
-			console.log("40", babyId);
 			deferred.resolve();
 		});
 		return deferred.promise;
 	}).then(function() {
 		var deferred = Q.defer();
 		if (parent1Id) {
-			console.log("47");
 			User.findByIdAndUpdate(parent1Id, {$push: {'parent.babies': babyId}}, function(err, parent) {
 				if (err) return res.status(500).send(err);
-				console.log("49", parent);
 				deferred.resolve();
 			});
 		} else {
-			console.log("54");
 			var newParent1 = new User();
 			newParent1.roles.push("parent");
 			newParent1.name = req.body.parent1.name;
@@ -67,14 +57,11 @@ exports.makeBaby = function (req, res) {
 			newParent1.password = "test";
 			newParent1.parent.access = "parent";
 			newParent1.parent.babies.push(babyId);
-			console.log("60", newParent1);
 			newParent1.save(function(err, parent) {
 				if (err) return res.status(500).send(err);
 				parent1Id = parent._id;
-				console.log("6	3", parent1Id);
 				Baby.findByIdAndUpdate(babyId, {$push: {'parents': parent1Id}}, function(err, baby) {
 					if (err) return res.status(500).send(err);
-					console.log("66", baby);
 					deferred.resolve();
 				});
 			});
@@ -85,7 +72,6 @@ exports.makeBaby = function (req, res) {
 		if (parent2Id) {
 			User.findByIdAndUpdate(parent2Id, {$push: {'parent.babies': babyId}}, function(err, parent) {
 				if (err) return res.status(500).send(err);
-				console.log("77", parent);
 				deferred.resolve();
 			});
 		} else {
@@ -100,10 +86,8 @@ exports.makeBaby = function (req, res) {
 				newParent2.save(function(err, parent) {
 					if (err) return res.status(500).send(err);
 					parent2Id = parent._id;
-					console.log("91", parent2Id);
 					Baby.findByIdAndUpdate(babyId, {$push: {'parents': parent2Id}}, function(err, baby) {
 						if (err) res.status(500).send(err);
-						console.log("94", baby);
 						deferred.resolve();
 					});
 				});
@@ -115,7 +99,6 @@ exports.makeBaby = function (req, res) {
 	}).then(function() {
 		Baby.findById(babyId, function(err, baby) {
 			if (err) return res.status(500).send(err);
-			console.log("103", baby);
 			res.send(baby);
 		});
 	});
@@ -129,7 +112,6 @@ exports.getBabies = function (req, res) {
 		if (err) {
 			return res.status(500).send(err);
 		}
-		console.log(result);
 		res.send(result);
 	});
 };
@@ -146,13 +128,13 @@ exports.getBaby = function (req, res) {
 		if (err) {
 			return res.status(500).send(err);
 		}
-		console.log(result);
 		res.send(result);
 	});
 };
 
 exports.editBaby = function(req, res) {
-	var parent1, parent2, baby;
+	console.log(req.body.parent2);
+	var baby, babyUpdated;
 	(function findBaby() {
 		var deferred = Q.defer();
 		Baby.findById(req.body.baby._id)
@@ -162,50 +144,99 @@ exports.editBaby = function(req, res) {
 			baby = response;
 			deferred.resolve();
 		});
+		return deferred.promise;
 	}()).then(function() {
 		var deferred = Q.defer();
-		if (baby.parents[0] === req.body.parent1) {
-			if (baby.parents[1] === req.body.parent2) {
+		if (baby.parents[0].email === req.body.parent1.email && baby.parents[0].name === req.body.parent1.name) {
+			if (baby.parents[1].email === req.body.parent2.email && baby.parents[1].name === req.body.parent2.name) {
+				//Both parents match db, only baby changed
 				Baby.findByIdAndUpdate(baby._id, req.body.baby, {new: true}, function(err, newBaby) {
 					if (err) return res.status(500).send(err);
 					res.send(newBaby);
+					babyUpdated = true;
+					deferred.resolve();
+				});
+			} else {
+				deferred.resolve();
+			}
+		} else {
+			deferred.resolve();
+		}
+		return deferred.promise;
+	}).then(function() {
+		//Parent1 Stuff
+		var deferred = Q.defer();
+		if (baby.parents[0].email === req.body.parent1.email && baby.parents[0].name === req.body.parent1.name) {
+			deferred.resolve();
+		} else {
+			if (!req.body.parent1.email && !req.body.parent1.name) {
+				//Parent1 is deleted
+				(function babyUpdate() {
+					var deferred2 = Q.defer();
+					Baby.findByIdAndUpdate(baby._id, {$pull: {'parents': baby.parents[0]}}, {new: true}, function(err, result) {
+						if (err) return res.status(500).send(err);
+						deferred2.resolve();
+					});
+					return deferred2.promise;
+				}()).then(function() {
+					var deferred2 = Q.defer();
+					User.findByIdAndUpdate(baby.parents[0]._id, {$pull: {'parent.babies': {_id: baby._id}}}, {new: true}, function(err, result) {
+						if (err) return res.status(500).send(err);
+						deferred2.resolve();
+					});
+				}).then(function() {
+					deferred.resolve();
+				});
+			} else {
+				//Parent1 Changed
+				User.findByIdAndUpdate(baby.parents[0]._id, req.body.parent1, {new: true}, function(err, result) {
+					if (err) return res.status(500).send(err);
 					deferred.resolve();
 				});
 			}
+		}
+		return deferred.promise;
+	}).then(function() {
+		//Parent2 Stuff
+		var deferred = Q.defer();
+		if (baby.parents[1].email === req.body.parent2.email && baby.parents[1].name === req.body.parent2.name) {
+			deferred.resolve();
 		} else {
-			var updateParent1 = function() {
-				var deferredness = Q.defer();
-				if (!req.body.parent1.email && !req.body.parent1.name) {
-					baby.parents.pull(baby.parents[0]);
-					User.findByIdAndUpdate(baby.parents[0]._id, {$pull: {'parent.babies': {_id: baby._id}}}, {new: true}, function(err, result) {
+			if (req.body.parent2.email === "" && req.body.parent2.name === "") {
+				//Parent2 is deleted
+				(function babyUpdate() {
+					var deferred2 = Q.defer();
+					console.log("224");
+					Baby.findByIdAndUpdate(baby._id, {$pull: {'parents': baby.parents[1]}}, {new: true}, function(err, result) {
 						if (err) return res.status(500).send(err);
-						deferredness.resolve();
+						deferred2.resolve();
 					});
-				} else {
-					User.findByIdAndUpdate(baby.parents[0]._id, req.body.parent1, {new: true}, function(err, result) {
+					return deferred2.promise;
+				}()).then(function() {
+					var deferred2 = Q.defer();
+					User.findByIdAndUpdate(baby.parents[1]._id, {$pull: {'parent.babies': {_id: baby._id}}}, {new: true}, function(err, result) {
 						if (err) return res.status(500).send(err);
-						parent1 = result;
-						deferredness.resolve();
+						deferred2.resolve();
 					});
-				}
-			};
-			if (baby) {
-
+				}).then(function() {
+					deferred.resolve();
+				});
+			} else {
+				//Parent2 Changed
+				User.findByIdAndUpdate(baby.parents[1]._id, req.body.parent2, {new: true}, function(err, result) {
+					if (err) return res.status(500).send(err);
+					deferred.resolve();
+				});
 			}
 		}
+		return deferred.promise;
+	}).then(function() {
+		//Update and return Baby
+		if(!babyUpdated) {
+			Baby.findByIdAndUpdate(baby._id, req.body.baby, {new: true}, function(err, result) {
+				if (err) return res.status(500).send(err);
+				res.send(result);
+			});
+		}
 	});
-};
-
-var updateParent2 = function() {
-	if (!req.body.parent2.email && !req.body.parent2.name) {
-		baby.parents.pull(baby.parents[1]);
-		User.findByIdAndUpdate(baby.parents[1]._id, {$pull: {'parent.babies': {_id: baby._id}}}, {new: true}, function(err, result) {
-			if (err) return res.status(500).send(err);
-		});
-	} else {
-		User.findByIdAndUpdate(baby.parents[1]._id, req.body.parent2, {new: true}, function(err, result) {
-			if (err) return res.status(500).send(err);
-			parent2 = result;
-		});
-	}
 };
