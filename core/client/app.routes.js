f86bbacbf69cf6d85e0a5099c2c1504e41cbacca
BrendanDayley <brendandayley@gmail.com>
2015-09-17 21:22:16 -0600

    fixed an error in the routes

--- core/client/app.routes.js
+++ core/client/app.routes.js
@@ -11,7 +11,8 @@
                templateUrl: 'components/home/home-template.html',
                controller: 'homeController'
             })
-            .state('medical', {
+         ////////////   medical /////////////////////////
+         .state('medical', {
                url: '/medical',
                templateUrl: 'components/nurse/nurse-template.html',
                controller: 'nurseController'
@@ -53,7 +54,9 @@
                templateUrl: 'components/nurse/nurse.makeBabyTmpl.html',
                controller: 'makeBabyCtrl'
             })
-            .state('hospital', {
+
+         ////////////   HOSPITAL /////////////////////////
+         .state('hospital', {
                url: '/hospital',
                templateUrl: 'components/hospital/hospital_view.html',
             })
@@ -78,7 +81,9 @@
                   }
                }
             })
-            .state('parent', {
+
+         ////////////   parent /////////////////////////
+         .state('parent', {
                url: '/parent',
                templateUrl: 'components/parent/parentView.html',
                controller: 'parentViewCtrl'
@@ -86,130 +91,23 @@
             .state('parent.settings', {
                url: '/settings',
                templateUrl: 'components/parent/parentSettings.html',
-               //  controller: 'parentSettingsCtrl'
-            })
-            .state('parent.create_note', {
-               url: '/create_note/:id',
-               templateUrl: 'components/parent/parentCreateNote.html',
-               controller: 'parentCreateNoteCtrl',
-               resolve: {
-                  promised: function (parentService, $stateParams) {
-                     var id = $stateParams.id;
-                     return parentService.getBabyById(id);
-                  }
-               }
+               controller: 'parentSettingsCtrl'
             })
             .state('parent.timeline', {
                url: '/timeline',
                templateUrl: 'components/parent/parentTimeline.html',
-               controller: 'parentTimelineCtrl',
+               controller: 'parentTimelineCtrl'
+            })
+            .state('parent.create_note', {
+               url: '/create_note/:id',
+               templateUrl: 'components/parent/parentCreateNote.html',
+               controller: 'parentNoteController',
                resolve: {
-                  promised: function (parentService, $stateParams) {
+                  promised: function (NurseService, $stateParams) {
                      var id = $stateParams.id;
-                     return parentService.getBabyById(id);
+                     return NurseService.getBabyById(id);
                   }
                }
             })
       })
-
-   ////////////   medical /////////////////////////
-   .state('medical', {
-         url: '/medical',
-         templateUrl: 'components/nurse/nurse-template.html',
-         controller: 'nurseController'
-      })
-      .state('medical.edit', {
-         url: '/edit/:id',
-         templateUrl: 'components/nurse/nurse.makeBabyTmpl.html',
-         controller: 'editController',
-         resolve: {
-            promised: function (NurseService, $stateParams) {
-               var id = $stateParams.id;
-               return NurseService.getBabyById(id);
-            }
-         }
-      })
-      .state('medical.search', {
-         url: '/search',
-         templateUrl: 'components/nurse/search_baby.html',
-         controller: 'nurseSearchCtrl',
-         resolve: {
-            getBabies: function (NurseService) {
-               return NurseService.getBabies();
-            }
-         }
-      })
-      .state('medical.create_note', {
-         url: '/create_note/:id',
-         templateUrl: 'components/nurse/create_note.html',
-         controller: 'noteController',
-         resolve: {
-            promised: function (NurseService, $stateParams) {
-               var id = $stateParams.id;
-               return NurseService.getBabyById(id);
-            }
-         }
-      })
-      .state('medical.create_account', {
-         url: '/account',
-         templateUrl: 'components/nurse/nurse.makeBabyTmpl.html',
-         controller: 'makeBabyCtrl'
-      })
-
-   ////////////   HOSPITAL /////////////////////////
-   .state('hospital', {
-         url: '/hospital',
-         templateUrl: 'components/hospital/hospital_view.html',
-      })
-      .state('hospital.search', {
-         url: '/search',
-         templateUrl: 'components/hospital/hospital_search.html',
-         controller: 'hospitalSearchCtrl'
-      })
-      .state('hospital.add_nurse', {
-         url: '/add_nurse',
-         templateUrl: 'components/hospital/hospital_add_nurse.html',
-         controller: 'hospitalAddNurseCtrl'
-      })
-      .state('hospital.edit_nurse', {
-         url: '/edit/:id',
-         templateUrl: 'components/hospital/hospital_add_nurse.html',
-         controller: 'hospitalEditNurseCtrl',
-         resolve: {
-            promised: function (hospitalSvc, $stateParams) {
-               var id = $stateParams.id;
-               return hospitalSvc.getOneStaff(id);
-            }
-         }
-      })
-
-   ////////////   parent /////////////////////////
-   .state('parent', {
-         url: '/parent',
-         templateUrl: 'components/parent/parentView.html',
-         controller: 'parentViewCtrl'
-      })
-      .state('parent.settings', {
-         url: '/settings',
-         templateUrl: 'components/parent/parentSettings.html',
-         controller: 'parentSettingsCtrl'
-      })
-      .state('parent.timeline', {
-         url: '/timeline',
-         templateUrl: 'components/parent/parentTimeline.html',
-         controller: 'parentTimelineCtrl'
-      })
-      .state('parent.create_note', {
-         url: '/create_note/:id',
-         templateUrl: 'components/parent/parentCreateNote.html',
-         controller: 'parentNoteController',
-         resolve: {
-            promised: function (NurseService, $stateParams) {
-               var id = $stateParams.id;
-               return NurseService.getBabyById(id);
-            }
-         }
-      })
-});
-
 }());
