b7c68904b4a9f81f070b8dccb0281ec48b21002d
Miles Peterken <mpeterken43@yahoo.com>
2015-09-18 17:27:20 -0600

    changes to css, and parent create note controller

--- core/client/components/parent/parentCreateNoteCtrl.js
+++ core/client/components/parent/parentCreateNoteCtrl.js
@@ -9,47 +9,26 @@
          $scope.theBaby = promised;
          $scope.images = [];
 
-      $scope.floatTheModal = function() {
-          $mdDialog.show({
-            templateUrl: "./components/modal-templates/parentAddNoteConfirmationModal.html",
-            locals: {
-              baby: response,
-              theBaby: $scope.theBaby
-            },
-            controller: "parentAddNoteConfirmationModalCtrl",
-          });
-      };
 
-      $scope.addBabyNote = function () {
-        $scope.note.baby = promised._id;
-        $scope.note.stats.heartRate = parseInt($scope.note.stats.heartRate);
-        $scope.note.stats.oxygen = parseInt($scope.note.stats.oxygen);
-        $scope.note.picturesUrl = $scope.imageId;
-        parentService.addBabyNote($scope.note).
-          then(function (response) {
+         $scope.addBabyNote = function () {
+            $scope.note.baby = promised._id;
+            $scope.note.stats.heartRate = parseInt($scope.note.stats.heartRate);
+            $scope.note.stats.oxygen = parseInt($scope.note.stats.oxygen);
+            $scope.note.picturesUrl = $scope.imageId;
+            parentService.addBabyNote($scope.note).
+            then(function (response) {
+               $mdDialog.show({
+                  templateUrl: "./components/modal-templates/parentAddNoteConfirmationModal.html",
+                  locals: {
+                     baby: response,
+                     theBaby: $scope.theBaby
+                  },
+                  controller: "parentAddNoteConfirmationModalCtrl",
+               });
+            })
 
-          })
-      };
-// =======
-//          $scope.addBabyNote = function () {
-//             $scope.note.baby = promised._id;
-//             $scope.note.stats.heartRate = parseInt($scope.note.stats.heartRate);
-//             $scope.note.stats.oxygen = parseInt($scope.note.stats.oxygen);
-//             $scope.note.picturesUrl = $scope.imageId;
-//             parentService.addBabyNote($scope.note).
-//             then(function (response) {
-//                $mdDialog.show({
-//                   templateUrl: "./components/modal-templates/parentAddNoteConfirmationModal.html",
-//                   locals: {
-//                      baby: response,
-//                      theBaby: $scope.theBaby
-//                   },
-//                   controller: "parentAddNoteConfirmationModalCtrl",
-//                });
-//             })
-//
-//          };
-// >>>>>>> master
+         };
+
 
          $scope.clearFields = function () {
             $scope.note = {};
