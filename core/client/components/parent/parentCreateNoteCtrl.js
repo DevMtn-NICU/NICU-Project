7e46360cacb63b3bf04d9e137711ab5f1aaab28f
BrendanDayley <brendandayley@gmail.com>
2015-09-17 21:23:11 -0600

    probably my beautifier

--- core/client/components/parent/parentCreateNoteCtrl.js
+++ core/client/components/parent/parentCreateNoteCtrl.js
@@ -1,32 +1,30 @@
 (function () {
-<<<<<<< HEAD
    "use strict";
 
    angular.module('app')
-      .controller('parentNoteController', function ($scope, promised, parentService, $mdDialog) {
+      .controller('parentCreateNoteCtrl', function ($scope, promised, parentService, $mdDialog) {
+
+
 
          $scope.note = {};
          $scope.note.stats = {};
          $scope.theBaby = promised;
          $scope.images = [];
-         $scope.showNote = false;
 
-         $scope.addBabyNote = function (ev) {
+         $scope.addBabyNote = function () {
             $scope.note.baby = promised._id;
             $scope.note.stats.heartRate = parseInt($scope.note.stats.heartRate);
             $scope.note.stats.oxygen = parseInt($scope.note.stats.oxygen);
             $scope.note.picturesUrl = $scope.imageId;
-            NurseService.addBabyNote($scope.note).
+            parentService.addBabyNote($scope.note).
             then(function (response) {
                $mdDialog.show({
-                  templateUrl: "./components/modal-templates/addNoteConfirmationModal.html",
+                  templateUrl: "./components/modal-templates/parentAddNoteConfirmationModal.html",
                   locals: {
                      baby: response,
                      theBaby: $scope.theBaby
                   },
-                  controller: "addNoteConfirmationModalCtrl",
-                  targetEvent: ev,
-                  clickOutsideToClose: true
+                  controller: "parentAddNoteConfirmationModalCtrl",
                });
             })
 
@@ -36,48 +34,6 @@
             $scope.note = {};
          }
 
-
-
       })
 
 }());
-=======
-  "use strict";
-
-  angular.module('app')
-    .controller('parentCreateNoteCtrl', function ($scope, promised, parentService, $mdDialog) {
-
-
-
-      $scope.note = {};
-      $scope.note.stats = {};
-      $scope.theBaby = promised;
-      $scope.images = [];
-
-      $scope.addBabyNote = function () {
-        $scope.note.baby = promised._id;
-        $scope.note.stats.heartRate = parseInt($scope.note.stats.heartRate);
-        $scope.note.stats.oxygen = parseInt($scope.note.stats.oxygen);
-        $scope.note.picturesUrl = $scope.imageId;
-        parentService.addBabyNote($scope.note).
-          then(function (response) {
-              $mdDialog.show({
-                templateUrl: "./components/modal-templates/parentAddNoteConfirmationModal.html",
-                locals: {
-                  baby: response,
-                  theBaby: $scope.theBaby
-                },
-                controller: "parentAddNoteConfirmationModalCtrl",
-              });
-          })
-
-      };
-
-      $scope.clearFields = function() {
-          $scope.note = {};
-      }
-
-    })
-
-} ());
->>>>>>> master
