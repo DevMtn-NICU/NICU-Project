0353ae2163e5503bbf782386033fd8fe3cabfc00
BrendanDayley <brendandayley@gmail.com>
2015-09-21 14:43:33 -0600

    more functionality haha

--- core/client/components/parent/babyLandingCtrl.js
+++ core/client/components/parent/babyLandingCtrl.js
@@ -4,21 +4,23 @@
    angular.module('app')
       .controller('babyLanding',
          function ($scope, parentService, $mdDialog, $stateParams, $cookies, $rootScope) {
-           $scope.baby = {};
-           //watches for dropdown in parent scope to change
-           $scope.$on('babyChanged', function(e) {
-             if ($scope.$parent.currentBaby) {
-               $scope.baby = $scope.$parent.currentBaby;
-               $scope.getBaby($scope.baby._id);
-             }
-           });
+            $scope.baby = {};
+            //watches for dropdown in parent scope to change
+            $scope.$on('babyChanged', function (e) {
+               if ($scope.$parent.currentBaby) {
+                  $scope.baby = $scope.$parent.currentBaby;
+                  $scope.getBaby($scope.baby._id);
+               }
+            });
 
-           $scope.getBaby = function(babyId) {
-             parentService.getBabyById(babyId)
-              .then(function (baby) {
-                  $scope.baby = baby;
-                  $scope.notes = baby.notes;
-              });
+            $scope.getBaby = function (babyId) {
+               parentService.getBabyById(babyId)
+                  .then(function (baby) {
+                     $scope.baby = baby;
+                     $scope.notes = baby.notes;
+                  });
             };
+
+
          });
 }());
