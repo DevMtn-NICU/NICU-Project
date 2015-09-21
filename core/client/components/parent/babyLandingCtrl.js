1b508225f11b814f75a8dd82ed8983ca2cb18f7f
BrendanDayley <brendandayley@gmail.com>
2015-09-21 11:02:02 -0600

    merge fix for sure

--- core/client/components/parent/babyLandingCtrl.js
+++ core/client/components/parent/babyLandingCtrl.js
@@ -1,81 +1,35 @@
 (function () {
-      "use strict";
+   "use strict";
 
-<<<<<<< HEAD
-      angular.module('app')
-         .controller('babyLanding',
-            function ($scope, parentService, $mdDialog, $stateParams) {
-               var testUser = {
-                  "_id": "55f882ed9f20e18029a3a831",
-                  "email": "dontDeleteMeEver@gmail.com",
-                  "name": "test",
-                  "created_at": "2015-09-15T20:43:25.282Z",
-                  "parent": {
-                     "access": "parent",
-                     "babies": [
-                     "55f882ec9f20e18029a3a830",
-                     "55f883299f20e18029a3a833"
-                  ]
-                  },
-                  "roles": [
-                  "parent"
-               ],
-                  "__v": 0
-               }
-               $scope.notes = [];
-               parentService.getBabyById(testUser.parent.babies[1])
-                  .then(function (baby) {
-                        for (var i = (baby.notes.length - 1); i >= 0; i--) {
-                           var noteId = baby.notes[i]._id;
-
-                           parentService.getBabyNote(noteId)
-                              .then(function (note) {
-                                 $scope.notes.push(note);
-                                console.log('babyLandingCtrl $scope.notes[0]: ', $scope.notes[0].baby);
-                              })
-
-                              }
-                     
-                                    
-                                 
-                        });
-=======
+   << << << < HEAD
    angular.module('app')
       .controller('babyLanding',
-         function ($scope, parentService, $mdDialog, $stateParams, $cookies) {
-           $scope.babies = $cookies.getObject("parentObj").babies;
+         function ($scope, parentService, $mdDialog, $stateParams) {
 
-           $scope.note = {};
-           parentService.getBabyById($scope.babies[0])
-              .then(function (baby) {
-                  console.log('this loaded', baby);
-                  var note = baby.notes[0]._id;
-                  console.log(note);
+            $scope.babies = $cookies.getObject("parentObj").babies;
 
-                  parentService.getBabyNote(note)
-                    .then(function (note) {
-                        $scope.note = note;
+            $scope.notes = [];
+            parentService.getBabyById(testUser.parent.babies[1])
+               .then(function (baby) {
+                  for (var i = (baby.notes.length - 1); i >= 0; i--) {
+                     var noteId = baby.notes[i]._id;
 
-                        $scope.stats = $scope.note.stats;
-                        $scope.baby = $scope.note.baby;
-                        $scope.comment = $scope.note.comment;
-                        console.log($scope.note);
-                      });
-                      console.log($scope.note);
-                      return $scope.note;
-            });
->>>>>>> master
+                     parentService.getBabyNote(noteId)
+                        .then(function (note) {
+                           $scope.notes.push(note);
+                           console.log('babyLandingCtrl $scope.notes[0]: ', $scope.notes[0].baby);
+                        })
+
+                  }
 
 
-                     //            $scope.stats = $scope.note.stats;
-                     //            $scope.baby = $scope.note.baby;
-                     //            $scope.comment = $scope.note.comment;
+
+               });
 
 
-<<<<<<< HEAD
-                  })
-         }());
-=======
+            //            $scope.stats = $scope.note.stats;
+            //            $scope.baby = $scope.note.baby;
+            //            $scope.comment = $scope.note.comment;
+
          });
-}());
->>>>>>> master
+}()); >>> >>> > master
