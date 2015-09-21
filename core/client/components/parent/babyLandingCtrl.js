b43e18e0286344bf286d1f199ee5137154c90d61
Spencer Davies <spencerhdavies@gmail.com>
2015-09-21 13:56:14 -0600

    dropdown works on landing page

--- core/client/components/parent/babyLandingCtrl.js
+++ core/client/components/parent/babyLandingCtrl.js
@@ -3,34 +3,23 @@
 
    angular.module('app')
       .controller('babyLanding',
-         function ($scope, parentService, $mdDialog, $stateParams, $cookies) {
-           $scope.babies = $cookies.getObject("parentObj").babies;
+         function ($scope, parentService, $mdDialog, $stateParams, $cookies, $rootScope) {
+           $scope.baby = {};
+           $scope.$on('babyChanged', function(e) {
+             if ($scope.$parent.currentBaby) {
+               $scope.baby = $scope.$parent.currentBaby;
+               console.log($scope.baby);
+               $scope.getBaby($scope.baby._id);
+             }
+           });
 
            $scope.note = {};
-           parentService.getBabyById($scope.babies[0])
+           $scope.getBaby = function(babyId) {
+             parentService.getBabyById(babyId)
               .then(function (baby) {
-                  console.log('this loaded', baby);
-                  var note = baby.notes[0]._id;
-                  console.log(note);
-
-                  parentService.getBabyNote(note)
-                    .then(function (note) {
-                        $scope.note = note;
-
-                        $scope.stats = $scope.note.stats;
-                        $scope.baby = $scope.note.baby;
-                        $scope.comment = $scope.note.comment;
-                        console.log($scope.note);
-                      });
-                      console.log($scope.note);
-                      return $scope.note;
-            });
-
-
-            $scope.stats = $scope.note.stats;
-            $scope.baby = $scope.note.baby;
-            $scope.comment = $scope.note.comment;
-
-
+                  $scope.baby = baby;
+                  $scope.note = baby.notes[0];
+              });
+            };
          });
 }());
