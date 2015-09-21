3029711c7cbb399f744af32c25a6d579f7be4b99
Miles Peterken <mpeterken43@yahoo.com>
2015-09-20 21:05:44 -0600

    create note modal working in parent and nurse paths, refactored (files deleted)

--- core/client/components/nurse/note-controller.js
+++ core/client/components/nurse/note-controller.js
@@ -2,31 +2,43 @@
   "use strict";
 
   angular.module('app')
-    .controller('noteController', function ($scope, promised, NurseService, $mdDialog) {
+    .controller('noteController', function ($scope, promised, NurseService, $mdDialog, $state) {
 
-      $scope.note = {};
-      $scope.note.stats = {};
-      $scope.theBaby = promised;
-    //   $scope.theBaby._id = promised._id;
-      $scope.images = [];
+        $scope.note = {};
+        $scope.note.stats = {};
+        $scope.theBaby = promised;
+        $scope.images = [];
 
-      $scope.floatTheModal =function() {
-          $mdDialog.show({
-            templateUrl: "./components/modal-templates/addNoteConfirmationModal.html",
-            locals: {
-              theBaby: $scope.theBaby,
-              note: $scope.note
-            },
-            controller: "addNoteConfirmationModalCtrl",
-        })
-      };
+        // clear the note
+        $scope.clearFields = function () {
+           $scope.note = {};
+        }
 
-      $scope.clearFields = function() {
-          $scope.note = {};
-      }
+        // open modal
+        $scope.floatTheModal = function() {
+            $mdDialog.show({
+                templateUrl: "./components/modal-templates/addNoteConfirmationModal.html",
+                scope: $scope,
+                preserveScope: true
+            });
+        }
+        // close modal
+        $scope.hideModal = function() {
+            $mdDialog.hide();
+        }
+        // make Baby note
+        $scope.addBabyNote = function () {
+           $scope.note.baby = promised._id;
+           $scope.note.stats.heartRate = parseInt($scope.note.stats.heartRate);
+           $scope.note.stats.oxygen = parseInt($scope.note.stats.oxygen);
+           $scope.note.picturesUrl = $scope.imageId;
+           NurseService.addBabyNote($scope.note).
+           then(function (response) {
+               $scope.hideModal();
+               $state.go('medical.search')
+           })
+        };
 
-
-
-    })
+     })
 
 } ());
