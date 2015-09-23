e9534bbd1aae69e35b006b9ac7559e8d53cbccdc
Wilson Parrish <wilsonparrishmk2@gmail.com>
2015-09-23 12:04:43 -0600

    merge error fixign

--- core/client/services/login-service.js
+++ core/client/services/login-service.js
@@ -1,6 +1,6 @@
 angular.module('app')
    .service('LoginService', function ($http, $q, $state, $cookies) {
-      this.validateLogin = function (login) {
+      this.validateLogin =  function (login) {
          console.log(login);
          var defer = $q.defer();
          var url = '/user/login';
