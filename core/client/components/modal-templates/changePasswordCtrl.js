8367c423cbf0fb42bd58cc72cffa77a8840843c2
Spencer Davies <spencerhdavies@gmail.com>
2015-09-23 16:54:16 -0600

    parents can change passwords

---
+++ core/client/components/modal-templates/changePasswordCtrl.js
@@ -0,0 +1,28 @@
+(function () {
+  "use strict";
+  angular.module('app').controller('changePasswordCtrl', function ($scope, $mdDialog, parentService, theme) {
+    $scope.passwordsDontMatch = false;
+    $scope.success = false;
+    $scope.theme = theme;
+
+    $scope.close = function() {
+      $mdDialog.hide();
+    };
+
+    $scope.changePassword = function() {
+      if($scope.newPassword !== $scope.verifyPassword) {
+        $scope.passwordsDontMatch = true;
+        $scope.newPassword = "";
+        $scope.verifyPassword = "";
+      } else {
+        parentService.changePassword($scope.newPassword)
+        .then(function(user) {
+          $scope.success = true;
+          $scope.passwordsDontMatch = false;
+          $scope.newPassword = "";
+          $scope.verifyPassword = "";
+        });
+      }
+    };
+  });
+} ());
