f279904a43ca832fb89854130883914343ab8cee
BrendanDayley <brendandayley@gmail.com>
2015-09-23 12:44:28 -0600

    set up failed login to keep the login up and running

--- core/client/services/login-service.js
+++ core/client/services/login-service.js
@@ -1,41 +1,41 @@
 angular.module('app')
    .service('LoginService', function ($http, $q, $state, $cookies) {
       this.validateLogin = function (login) {
-         console.log(login);
-         var defer = $q.defer();
+         var deferred = $q.defer();
          var url = '/user/login';
          $http({
             method: 'POST',
             url: url,
             data: JSON.stringify(login),
          }).then(function (res) {
-            console.log('.then is responding');
-            console.log('response', res.data);
-            var role = res.data.roles;
-            console.log('role', role[0]);
-            var user = res.data;
-            $cookies.putObject("userId", user._id);
-            $cookies.putObject("userRoles", user.roles);
-            $cookies.putObject("userEmail", user.email);
-            $cookies.putObject("parentObj", user.parent);
-            $cookies.putObject("contactObj", user.contact);
-            $cookies.putObject("nurseObj", user.nurse);
-            if (role[0] === 'nurse') {
-               $state.go('medical.search');
+               var role = res.data.roles;
+               var user = res.data;
+               $cookies.putObject("userId", user._id);
+               $cookies.putObject("userRoles", user.roles);
+               $cookies.putObject("userEmail", user.email);
+               $cookies.putObject("parentObj", user.parent);
+               $cookies.putObject("contactObj", user.contact);
+               $cookies.putObject("nurseObj", user.nurse);
+               if (role[0] === 'nurse') {
+                  $state.go('medical.search');
 
-            } else if (role[0] === 'parent') {
-               console.log(res.data._id);
+               } else if (role[0] === 'parent') {
 
-               $state.go('parent.landing', {
-                  'user': user
-               });
-            } else if (role[0] === 'contact') {
+                  $state.go('parent.landing', {
+                     'user': user
+                  });
+               } else if (role[0] === 'contact') {
 
-               $state.go('parent.landing', {
-                  'user': user
-               });
+                  $state.go('parent.landing', {
+                     'user': user
+                  });
 
-            }
-         });
+               }
+            },
+            //this is the error handeling function
+            function (response) {
+               deferred.resolve(response);
+            })
+         return deferred.promise;
       };
    });
