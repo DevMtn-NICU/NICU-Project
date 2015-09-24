9c36523305838932b998e9b35ee4fac1e8d1d578
Spencer Davies <spencerhdavies@gmail.com>
2015-09-24 13:45:04 -0600

    merging master

--- core/client/services/contactService.js
+++ core/client/services/contactService.js
@@ -1,8 +1,10 @@
 (function () {
    "use strict";
-   angular.module("app").service("contactService", function ($http, $q) {
+   angular.module("app").service("contactService", function ($http, $q, $cookies) {
+     this.getFeed = function() {
+
+     };
 
 
-
-   })
+   });
 }());
