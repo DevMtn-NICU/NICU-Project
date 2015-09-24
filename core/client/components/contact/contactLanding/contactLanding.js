c7dc8f41d737a342e08b8604a016591317b127e8
BrendanDayley <brendandayley@gmail.com>
2015-09-24 16:08:37 -0600

    login now works for contacts... again

--- core/client/components/contact/contactLanding/contactLanding.js
+++ core/client/components/contact/contactLanding/contactLanding.js
@@ -3,7 +3,7 @@
 
    angular.module('app')
       .controller('contactLandingCtrl', function ($scope, $mdDialog, contactService) {
-
+         console.log('contact landing controll');
 
       });
 
