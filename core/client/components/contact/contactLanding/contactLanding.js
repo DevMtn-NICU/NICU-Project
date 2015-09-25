9a56d5601d24a6aea2a496413ecf15d59bb5ec4a
BrendanDayley <brendandayley@gmail.com>
2015-09-25 10:38:20 -0600

    set up the contact nested view to show, and added data to the controller

--- core/client/components/contact/contactLanding/contactLanding.js
+++ core/client/components/contact/contactLanding/contactLanding.js
@@ -4,6 +4,8 @@
    angular.module('app')
       .controller('contactLandingCtrl', function ($scope, $mdDialog, contactService) {
          console.log('contact landing controll');
+         $scope.baby = $scope.$parent.currentBaby;
+         console.log("current baby: ", $scope.$parent.currentBaby);
 
       });
 
