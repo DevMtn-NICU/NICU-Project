68cd8aa423a10112f82791959f7a218bf7830f37
Miles Peterken <mpeterken43@yahoo.com>
2015-09-25 20:54:02 -0600

    add note confirmation modal

--- core/client/components/nurse/note-controller.js
+++ core/client/components/nurse/note-controller.js
@@ -27,9 +27,11 @@
 
         // open modal
         $scope.floatTheModal = function() {
-            $scope.shortUrl = $scope.note.picturesUrl;
-            $scope.shortUrl = $scope.shortUrl.split('/');
-            $scope.shortUrl = $scope.shortUrl[$scope.shortUrl.length -1];
+            if ($scope.note.picturesUrl) {
+                $scope.shortUrl = $scope.note.picturesUrl;
+                $scope.shortUrl = $scope.shortUrl.split('/');
+                $scope.shortUrl = $scope.shortUrl[$scope.shortUrl.length -1];
+            };
 
             $mdDialog.show({
                 templateUrl: "./components/modal-templates/addNoteConfirmationModal.html",
