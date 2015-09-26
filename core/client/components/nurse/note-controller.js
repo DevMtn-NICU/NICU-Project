ec483936386302db4ae07856b9617abd369da34c
BrendanDayley <brendandayley@gmail.com>
2015-09-25 19:44:07 -0600

    added add note error handling

--- core/client/components/nurse/note-controller.js
+++ core/client/components/nurse/note-controller.js
@@ -1,46 +1,105 @@
 (function () {
-  "use strict";
+   "use strict";
 
-  angular.module('app')
-    .controller('noteController', function ($scope, promised, NurseService, $mdDialog, $state, $cookies) {
+   angular.module('app')
+      .controller('noteController', function ($scope, promised, NurseService, $mdDialog, $state, $cookies) {
 
-        $scope.note = {};
-        $scope.note.stats = {};
-        $scope.theBaby = promised;
-        $scope.images = [];
+         $scope.note = {};
+         $scope.note.stats = {};
+         $scope.theBaby = promised;
+         $scope.images = [];
 
-        // clear the note
-        $scope.clearFields = function () {
-           $scope.note = {};
-        }
+         // clear the note
+         $scope.clearFields = function () {
+            $scope.note = {};
+         }
 
-        // open modal
-        $scope.floatTheModal = function() {
-            $mdDialog.show({
-                templateUrl: "./components/modal-templates/addNoteConfirmationModal.html",
-                scope: $scope,
-                preserveScope: true
-            });
-        }
-        // close modal
-        $scope.hideModal = function() {
-            $mdDialog.hide();
-        }
-        // make Baby note
-        $scope.addBabyNote = function () {
+         // open modal
+         $scope.floatTheModal = function () {
+               $mdDialog.show({
+                  templateUrl: "./components/modal-templates/addNoteConfirmationModal.html",
+                  scope: $scope,
+                  preserveScope: true
+               });
+            }
+            // close modal
+         $scope.hideModal = function () {
+               $mdDialog.hide();
+            }
+            // make Baby note
+         $scope.addBabyNote = function () {
             console.log("note: ", $scope.note);
-           $scope.note.baby = promised._id;
-           $scope.note.stats.heartRate = parseInt($scope.note.stats.heartRate);
-           $scope.note.stats.oxygen = parseInt($scope.note.stats.oxygen);
-           $scope.note.creator = $cookies.getObject('name');
-        //    $scope.note.picturesUrl = $scope.note.picturesUrl;
-           NurseService.addBabyNote($scope.note).
-           then(function (response) {
-               $scope.hideModal();
-               $state.go('medical.search')
-           })
-        };
+            $scope.note.baby = promised._id;
+            $scope.note.stats.heartRate = parseInt($scope.note.stats.heartRate);
+            $scope.note.stats.oxygen = parseInt($scope.note.stats.oxygen);
+            $scope.note.creator = $cookies.getObject('name');
+            if (!$scope.note.stats.oxygen) {
+               delete($scope.note.stats.oxygen);
+            }
+            if (!$scope.note.stats.heartRate) {
+               console.log('number');
+               delete($scope.note.stats.heartRate);
+               //console.log('oxy', $scope.note.stats.heartRate);
+            }
+            //TODO add note pictures
+            var validNote = false;
 
-     })
+            //            if ($scope.note.stats.heartRate > 0 || $scope.note.stats.heartRate < 0) {
+            //               console.log('true');
+            //               validNote = true;
+            //            } 
+            //            else
+            if ($scope.note.stats.other > 0) {
+               console.log('true');
+               validNote = true;
+            }
+            //            else if ($scope.note.stats.oxygen > 0 || $scope.note.stats.oxygen < 0) {
+            //               console.log('true');
+            //               validNote = true;
+            //            } 
+            else if ($scope.note.stats.bathed) {
+               console.log('true');
+               validNote = true;
+            } else if ($scope.note.stats.bloodPressure) {
+               console.log('true');
+               validNote = true;
+            } else if ($scope.note.stats.changed) {
+               console.log('true');
+               validNote = true;
+            } else if ($scope.note.stats.fed) {
+               console.log('true');
+               validNote = true;
+            } else if ($scope.note.stats.weight) {
+               console.log('true');
+               validNote = true;
+            } else if ($scope.note.picturesUrl) {
+               console.log('true');
+               validNote = true;
+            } else if ($scope.note.comment) {
+               console.log('true');
+               validNote = true;
+            } else if ($scope.note.stats.heartRate) {
+               validNote = true;
+            } else if ($scope.note.stats.oxygen) {
+               validNote = true;
+            } else {
+               validNote = false;
+            }
 
-} ());
+            if (validNote) {
+               NurseService.addBabyNote($scope.note).
+               then(function (response) {
+                  $scope.hideModal();
+                  $state.go('medical.search')
+               })
+            } else {
+               console.log('failed');
+            }
+
+
+
+         };
+
+      })
+
+}());
--- core/server/models/babyNote.server.model.js
+++ core/server/models/babyNote.server.model.js
@@ -1,25 +1,63 @@
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+   Schema = mongoose.Schema;
 
 var noteSchema = new Schema({
-  baby: {type: Schema.Types.ObjectId, ref: "Baby", required: true},
-  picturesUrl: {type: String},
-  stats: {
-    bloodPressure: {type: String},
-    heartRate: {type: Number, min: 0},
-    oxygen: {type: Number, min: 0},
-    weight: {type: String},
-    other: [{
-      stat: {type: String},
-      value: {type: String}
+   baby: {
+      type: Schema.Types.ObjectId,
+      ref: "Baby",
+      required: true
+   },
+   picturesUrl: {
+      type: String
+   },
+   stats: {
+      bloodPressure: {
+         type: String
+      },
+      heartRate: {
+         type: Number,
+         min: 0
+      },
+      oxygen: {
+         type: Number,
+         min: 0
+      },
+      weight: {
+         type: String
+      },
+      other: [{
+         stat: {
+            type: String
+         },
+         value: {
+            type: String
+         }
     }],
-    fed: {type: Boolean, default: false},
-    changed: {type: Boolean, default: false},
-    bathed: {type: Boolean, default: false}
-  },
-  comment: {type: String},
-  created_at: {type: Date, default: Date.now, required: true},
-  creator: {type: Schema.Types.ObjectId, ref: "User" }
+      fed: {
+         type: Boolean,
+         default: false
+      },
+      changed: {
+         type: Boolean,
+         default: false
+      },
+      bathed: {
+         type: Boolean,
+         default: false
+      }
+   },
+   comment: {
+      type: String
+   },
+   created_at: {
+      type: Date,
+      default: Date.now,
+      required: true
+   },
+   creator: {
+      type: Schema.Types.ObjectId,
+      ref: "User"
+   }
 });
 
 module.exports = mongoose.model('Note', noteSchema);
