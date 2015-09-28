81f7a4eaf4edddfecd9801fe90adc8ee55b39b4d
CodeMaster7 <italianstalian_2@yahoo.com>
2015-09-28 17:25:21 -0600

    image carousel

--- core/client/components/image-slider/slider.js
+++ core/client/components/image-slider/slider.js
@@ -20,5 +20,11 @@ angular.module('app')
             })
       }
       
-      
+      $scope.slides = [
+            {image: 'nurse6.png', description: 'Image 00'},
+            {image: 'images/img01.jpg', description: 'Image 01'},
+            {image: 'images/img02.jpg', description: 'Image 02'},
+            {image: 'images/img03.jpg', description: 'Image 03'},
+            {image: 'images/img04.jpg', description: 'Image 04'}
+        ];
    })
