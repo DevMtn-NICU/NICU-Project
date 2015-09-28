4ca3f8d886b97b2f75426bf7ecfa264db27755fd
Wilson Parrish <wilsonparrishmk2@gmail.com>
2015-09-28 12:13:31 -0600

    added the weight graph, moved to a GRAPHS tab

--- core/client/components/parent/parentTimelineCtrl.js
+++ core/client/components/parent/parentTimelineCtrl.js
@@ -1,94 +1,129 @@
 (function () {
-   "use strict";
+      "use strict";
 
-   angular.module('app')
-      .controller('parentTimelineCtrl', function ($scope, parentService, $mdDialog, $cookies, $rootScope) {
+      angular.module('app')
+            .controller('parentTimelineCtrl', function ($scope, parentService, $mdDialog, $cookies, $rootScope) {
 
-         $scope.baby = {};
-         $scope.images = [];
+                  $scope.baby = {};
+                  $scope.images = [];
 
-         function getCurrentBaby() {
-            var babyId = parentService.sendBabyId();
-            if (babyId) {
-               parentService.getBabyById(babyId)
-                  .then(function (baby) {
-                     $scope.baby = baby;
-                     $scope.notes = baby.notes;
-                     $scope.comments = baby.comments;
-                     $scope.$broadcast('babyChanged');
-                  })
-               return $scope.baby, $scope.notes;
-            }
-            console.log('getCurrentBaby failed');
-         }
-         getCurrentBaby();
-
-
-
-         $scope.$on('babyChanged', function (e) {
-            if ($scope.$parent.currentBaby) {
-               $scope.baby = $scope.$parent.currentBaby;
-               console.log($scope.baby);
-               for (var j = 0; j < $scope.baby.notes.length; j++) { //date parsing
-                  $scope.baby.notes[j].created_at = new Date($scope.baby.notes[j].created_at).toLocaleString();
-                  console.log($scope.baby.notes[j].picturesUrl);
-                  if ($scope.baby.notes[j].picturesUrl) {
-                     $scope.images.push($scope.baby.notes[j].picturesUrl)
-
-
+                  function getCurrentBaby() {
+                        var babyId = parentService.sendBabyId();
+                        if (babyId) {
+                              parentService.getBabyById(babyId)
+                                    .then(function (baby) {
+                                          $scope.baby = baby;
+                                          $scope.notes = baby.notes;
+                                          $scope.comments = baby.comments;
+                                          $scope.$broadcast('babyChanged');
+                                    })
+                              return $scope.baby, $scope.notes;
+                        }
+                        console.log('getCurrentBaby failed');
                   }
-               }
-               $scope.numberOfImages = $scope.images.length;
-               (function configureChartData() {
-                  $scope.data.data = [];
-                  console.log($scope.baby.notes.length);
-                  for (var i = ($scope.baby.notes.length - 1); i > ($scope.baby.notes.length - 6); i--) {
-                     console.log(i);
-                     var note = $scope.baby.notes[i];
-                     $scope.data.data.unshift({
-                        x: new Date(note.created_at).toLocaleTimeString(),
-                        y: [note.stats.heartRate, note.stats.oxygen]
-                     });
-                     console.log($scope.data.data);
-                  }
-               }());
-            }
-         });
+                  getCurrentBaby();
 
-         $scope.config = {
-            title: 'Stats Over Time', // chart title. If this is false, no title element will be created.
-            tooltips: true,
-            labels: false, // labels on data points
-            // exposed events
-            mouseover: function () {},
-            mouseout: function () {},
-            click: function () {},
-            // legend config
-            legend: {
-               display: true, // can be either 'left' or 'right'.
-               position: 'right',
-               // you can have html in series name
-               htmlEnabled: false
-            },
-            // override this array if you're not happy with default colors
-            colors: ['red', 'blue'],
-            lineLegend: 'lineEnd', // Only on line Charts
-            lineCurveType: 'monotone', // change this as per d3 guidelines to avoid smoothline
-            isAnimate: true, // run animations while rendering chart
-            yAxisTickFormat: 's', //refer tickFormats in d3 to edit this value
-            xAxisMaxTicks: 5, // Optional: maximum number of X axis ticks to show if data points exceed this number
-            waitForHeightAndWidth: false // if true, it will not throw an error when the height or width are not defined (e.g. while creating a modal form), and it will be keep watching for valid height and width values
-         };
 
-         $scope.data = {
-            series: ["Heart Rate", "Oxygen(%)"],
-            data: [] //being populated by the function on baby select
-         };
 
-         console.log($scope.baby);
+                  $scope.$on('babyChanged', function (e) {
+                        if ($scope.$parent.currentBaby) {
+                              $scope.baby = $scope.$parent.currentBaby;
+                              console.log($scope.baby);
+                              for (var j = 0; j < $scope.baby.notes.length; j++) { //date parsing
+                                    $scope.baby.notes[j].created_at = new Date($scope.baby.notes[j].created_at).toLocaleString();
+                                    console.log($scope.baby.notes[j].picturesUrl);
+                                    if ($scope.baby.notes[j].picturesUrl) {
+                                          $scope.images.push($scope.baby.notes[j].picturesUrl)
 
-      })
-}());
+
+                                    }
+                              }
+                              $scope.numberOfImages = $scope.images.length;
+                              (function configureChartData() {
+                                    $scope.data.data = [];
+                                    console.log($scope.baby.notes.length);
+                                    for (var i = ($scope.baby.notes.length - 1); i > ($scope.baby.notes.length - 6); i--) {
+                                          console.log(i);
+                                          var note = $scope.baby.notes[i];
+                                          $scope.data.data.unshift({
+                                                x: new Date(note.created_at).toLocaleTimeString(),
+                                                y: [note.stats.heartRate, note.stats.oxygen]
+                                          });
+                                          $scope.wtData.data.unshift({
+                                                x: new Date(note.created_at).toLocaleTimeString(),
+                                                y: [parseInt(note.stats.weight)]
+                                          });
+                                          console.log($scope.data.data);
+                                    }
+                              } ());
+                        }
+                  });
+
+                  $scope.config = {
+                        title: 'Stats Over Time', // chart title. If this is false, no title element will be created.
+                        tooltips: true,
+                        labels: false, // labels on data points
+                        // exposed events
+                        mouseover: function () { },
+                        mouseout: function () { },
+                        click: function () { },
+                        // legend config
+                        legend: {
+                              display: true, // can be either 'left' or 'right'.
+                              position: 'right',
+                              // you can have html in series name
+                              htmlEnabled: false
+                        },
+                        // override this array if you're not happy with default colors
+                        colors: ['red', 'blue'],
+                        lineLegend: 'lineEnd', // Only on line Charts
+                        lineCurveType: 'monotone', // change this as per d3 guidelines to avoid smoothline
+                        isAnimate: true, // run animations while rendering chart
+                        yAxisTickFormat: 's', //refer tickFormats in d3 to edit this value
+                        xAxisMaxTicks: 5, // Optional: maximum number of X axis ticks to show if data points exceed this number
+                        waitForHeightAndWidth: false // if true, it will not throw an error when the height or width are not defined (e.g. while creating a modal form), and it will be keep watching for valid height and width values
+                  };
+
+                  $scope.data = {
+                        series: ["Heart Rate", "Oxygen(%)"],
+                        data: [] //being populated by the function on baby select
+                  };
+         
+                  //This is the weight graph config stuff
+                  $scope.wtConfig = {
+                        title: 'Weight', // chart title. If this is false, no title element will be created.
+                        tooltips: true,
+                        labels: false, // labels on data points
+                        // exposed events
+                        mouseover: function () { },
+                        mouseout: function () { },
+                        click: function () { },
+                        // legend config
+                        legend: {
+                              display: true, // can be either 'left' or 'right'.
+                              position: 'right',
+                              // you can have html in series name
+                              htmlEnabled: false
+                        },
+                        // override this array if you're not happy with default colors
+                        colors: ['orange', 'blue'],
+                        lineLegend: 'lineEnd', // Only on line Charts
+                        lineCurveType: 'monotone', // change this as per d3 guidelines to avoid smoothline
+                        isAnimate: true, // run animations while rendering chart
+                        yAxisTickFormat: 's', //refer tickFormats in d3 to edit this value
+                        xAxisMaxTicks: 5, // Optional: maximum number of X axis ticks to show if data points exceed this number
+                        waitForHeightAndWidth: false // if true, it will not throw an error when the height or width are not defined (e.g. while creating a modal form), and it will be keep watching for valid height and width values
+                  };
+
+                  $scope.wtData = {
+                        series: ["Weight(g)"],
+                        data: [] //being populated by the function on baby select
+                  };
+
+                  console.log($scope.baby);
+
+            })
+} ());
 
 // ZOMBIE CODE TO WORK ON LATER PERHAPS
 // for (var i = ($scope.baby.notes.length - 1); i > ($scope.baby.notes.length - 6); i--) {
