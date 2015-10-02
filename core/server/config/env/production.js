6e092f98a8b74abbedcd79dae2d0c172c0af7aff
Wilson Parrish <wilsonparrishmk2@gmail.com>
2015-10-02 14:41:42 -0600

    please work

--- core/server/config/env/production.js
+++ core/server/config/env/production.js
@@ -3,5 +3,5 @@ module.exports = {
    // you want to get your own mongolab database and credentials from within Heroku
    // if you leave it like this you will be hitting my database :)
    db: 'mongodb://nicu-user:makebaby@ds033317.mongolab.com:33317/nicu-project',
-   sessionSecret: 'nicugroupdevF%$#no'
+   sessionSecret: 'nicugroupdevF%$#yeah'
 };
